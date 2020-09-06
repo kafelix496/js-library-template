5090692c26f345c57dfb9655e174732237fac122
Felix J Lee <ka.felix496@gmail.com>
2020-09-06 18:54:52 -0400

    update rollup config to manage easily

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,63 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
-const common = {
-  input: 'src/index.js',
-  external: [],
+const common = (isProd) => ({
   plugins: [
     resolve(),
     babel({
       exclude: 'node_modules/**',
     }),
-    process.env.BUILD === 'production' && terser(),
+    isProd && terser(),
   ],
-};
+});
 
-export default {
-  ...common,
-};
+const results = (fileName, umdName, external) => [{
+  input: `src/${fileName}/index.js`,
+  external,
+  output: {
+    file: `es/${fileName}.js`,
+    format: 'es',
+  },
+}, {
+  input: `src/${fileName}/index.js`,
+  external,
+  output: {
+    file: `es/${fileName}.min.js`,
+    format: 'es',
+  },
+}, {
+  input: `src/${fileName}/index.js`,
+  external,
+  output: {
+    file: `cjs/${fileName}.js`,
+    format: 'cjs',
+  },
+}, {
+  input: `src/${fileName}/index.js`,
+  external,
+  output: {
+    file: `cjs/${fileName}.min.js`,
+    format: 'cjs',
+  },
+}, {
+  input: `src/${fileName}/index.js`,
+  external,
+  output: {
+    file: `dist/${fileName}.js`,
+    format: 'umd',
+    name: umdName,
+  },
+}, {
+  input: `src/${fileName}/index.js`,
+  external,
+  output: {
+    file: `dist/${fileName}.min.js`,
+    format: 'umd',
+    name: umdName,
+  },
+}];
+
+export default results('customLibrary', 'CL', []).map((object) => ({
+  ...common(new RegExp(/.+min\.js$/).test(object.output.file)),
+  ...object,
+}));
