e4dd4537e3e133f39220e1cc395c82f642e9ce67
Kevin J Lee <ka.felix496@gmail.com>
2020-12-13 15:13:00 -0500

    Remove examples. I don't think it's needed in js library template.

--- examples/cjs/index.js
+++
@@ -1,4 +0,0 @@
-const { add, multiply } = require('@kafelix496/js-library-template')
-
-console.log('add(1, 3)', add(1, 3))
-console.log('multiply(2, 3)', multiply(2, 3))
--- scripts/initConfig.js
+++ scripts/initConfig.js
@@ -54,7 +54,6 @@ prompt([{
 
     const packageJsonString = fs.readFileSync('package.json', 'utf8')
     const packageJson = JSON.parse(packageJsonString)
-    const originalPackageName = packageJson.name
     packageJson.name = answer.name
     packageJson.description = answer.description
     packageJson.author = answer.author
@@ -62,40 +61,8 @@ prompt([{
     packageJson.unpkg = packageJson.unpkg.replace(originalFolderNameRegExp, answer.folderName)
     fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2))
 
-    const esAppjs = fs.readFileSync('examples/es/src/App.js', 'utf8')
-    fs.writeFileSync(
-      'examples/es/src/App.js',
-      esAppjs
-        .replace(new RegExp(originalPackageName, 'g'), packageJson.name)
-        .replace(originalFolderNameRegExp, answer.folderName)
-        .replace(/CUSTOM_UMD_GLOBAL_VARIABLE/g, answer.umdName)
-    )
-    const cjsIndexjs = fs.readFileSync('examples/cjs/index.js', 'utf8')
-    fs.writeFileSync(
-      'examples/cjs/index.js',
-      cjsIndexjs
-        .replace(new RegExp(originalPackageName, 'g'), packageJson.name)
-        .replace(originalFolderNameRegExp, answer.folderName)
-        .replace(/CUSTOM_UMD_GLOBAL_VARIABLE/g, answer.umdName)
-    )
-    const umdIndexHtml = fs.readFileSync('examples/umd/index.html', 'utf8')
-    fs.writeFileSync(
-      'examples/umd/index.html',
-      umdIndexHtml
-        .replace(new RegExp(originalPackageName, 'g'), packageJson.name)
-        .replace(originalFolderNameRegExp, answer.folderName)
-        .replace(/CUSTOM_UMD_GLOBAL_VARIABLE/g, answer.umdName)
-    )
-
-    console.log('===== Please wait a little bit... Installing npm packages (1 / 4) =====')
+    console.log('===== Please wait a little bit... Installing npm packages =====')
     execSync('npm install')
-    console.log('===== Please wait a little bit... Installing npm packages (2 / 4) =====')
-    execSync(`cd examples/es && npm remove ${originalPackageName} && npm install ../../`)
-    console.log('===== Please wait a little bit... Installing npm packages (3 / 4) =====')
-    execSync(`cd examples/cjs && npm remove ${originalPackageName} && npm install ../../`)
-    console.log('===== Please wait a little bit... Installing npm packages (4 / 4) =====')
-    execSync(`cd examples/umd && npm remove ${originalPackageName} && npm install ../../`)
-
     console.log('===== Please wait a little bit... Building packages =====')
     execSync('npm run build')
   } catch (err) {
